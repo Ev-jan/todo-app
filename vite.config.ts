11c57e45d22637aaa77d0ab5c9285a9c565e648f
Evgeny Mazyar <evgen.mazyar@gmail.com>
2025-04-03 15:24:23 +0400

    chore: update vite.config and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+  },
 });
