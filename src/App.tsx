cf2bd0aaf2067c84a53378caa0c5c5a57c5474d5
Evgeny Mazyar <evgen.mazyar@gmail.com>
2025-04-03 15:26:57 +0400

    feat: incorporate all components into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
-import Header from './components/Header'
+import { Header } from '@/components/Header'
+import { StatsBar } from './components/StatsBar'
+import { TaskInput } from './components/TaskInput/TaskInput'
+import { TodoList } from './components/TodoList/TodoList'
 
 function App() {
 
   return (
-    <>
+    <div className="min-h-screen bg-gray-200">
       <Header />
-    </>
+      <div className="min-h-screen flex items-start justify-center">
+        <div className="w-full max-w-xl shadow-lg bg-white rounded-sm border-2 border-gray-300 ">
+          <TaskInput />
+          <TodoList />
+          <StatsBar />
+        </div>
+      </div>
+    </div>
+
   )
 }
 
